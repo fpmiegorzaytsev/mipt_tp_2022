871a521d37ced54891e97527952f10e410cb5c72
fpmiegorzaytsev <zaitsev.egor@phystech.edu>
2022-04-21 19:31:24 +0300

    Reworked LeapTest and TreeTest classes in task4 solution

--- task4/src/test/java/ru/tp/add_01/LeapTest.java
+++ task4/src/test/java/ru/tp/add_01/LeapTest.java
@@ -1,6 +1,5 @@
 package ru.tp.add_01;
 
-import org.junit.Assert;
 import org.junit.Test;
 import java.util.Arrays;
 
@@ -8,34 +7,63 @@ import static org.junit.Assert.*;
 
 public class LeapTest {
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testIsLeap() {
-
+    @Test
+    public void testIsLeap1() {
         assertTrue(Leap.isLeap(2000));
+    }
+
+    @Test
+    public void testIsLeap2() {
         assertTrue(Leap.isLeap(1984));
+    }
 
+    @Test
+    public void testIsLeap3(){
         assertFalse(Leap.isLeap(2100));
-        assertFalse(Leap.isLeap(2013));
+    }
 
-        Leap.isLeap(0);
+    @Test
+    public void testIsLeap4(){
+        assertFalse(Leap.isLeap(2013));
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testGetMothDay(){
+    public void testIsLeapForException(){
+        Leap.isLeap(0);
+    }
 
+
+    @Test
+    public void testGetMonthDay1(){
         for (int i = 1; i < 13; i++) {
             assertEquals(30, Leap.GetMonthDays(1930, i));
         }
+    }
 
+    @Test
+    public void testGetMonthDay2(){
         assertEquals(29, Leap.GetMonthDays(1984, 2));
-        assertEquals(28, Leap.GetMonthDays(1983, 2));
+    }
 
+    @Test
+    public void testGetMothDay3(){
+        assertEquals(28, Leap.GetMonthDays(1983, 2));
+    }
+
+    @Test
+    public void testGetMothDay4(){
         for (int month : Arrays.asList(4, 6, 9, 11)){
             assertEquals(30, Leap.GetMonthDays(2017, month));
-
         }
+    }
 
+    @Test
+    public void testGetMothDay5(){
         assertEquals(31, Leap.GetMonthDays(2017, 7));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetMothDayForException(){
         Leap.GetMonthDays(2017, 13);
     }
 }
--- task4/src/test/java/ru/tp/tree_02/TreeTest.java
+++ task4/src/test/java/ru/tp/tree_02/TreeTest.java
@@ -1,13 +1,43 @@
 package ru.tp.tree_02;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.NotDirectoryException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class TreeTest {
+
+    private Path highDirectoryPath;
+    private Path middleDirectoryPath;
+    private Path lowDirectoryPath;
+    private Path filePath;
+
+
+    @Before
+    public void setUp() throws IOException {
+        Path path = Paths.get("").toAbsolutePath();
+        highDirectoryPath = Files.createTempDirectory(path,"highDir");
+        middleDirectoryPath = Files.createTempDirectory(highDirectoryPath, "middleDir");
+        lowDirectoryPath = Files.createTempDirectory(middleDirectoryPath, "lowDir");
+        filePath = File.createTempFile("file", ".java", lowDirectoryPath.toFile()).toPath();
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        Files.delete(filePath);
+        Files.delete(lowDirectoryPath);
+        Files.delete(middleDirectoryPath);
+        Files.delete(highDirectoryPath);
+    }
 
     @Test(expected = FileNotFoundException.class)
     public void testForFileNotFoundException() throws NotDirectoryException, FileNotFoundException {
@@ -16,14 +46,23 @@ public class TreeTest {
 
     @Test(expected = NotDirectoryException.class)
     public void testForNotDirectoryException() throws NotDirectoryException, FileNotFoundException {
-        Tree.getTree("./src/test/java/ru/tp/tree_02/TreeTest.java", false);
+        Tree.getTree(filePath.toString(), false);
     }
 
     @Test
-    public void testForGetTreeMethod() throws NotDirectoryException, FileNotFoundException {
-        Tree.getTree("./src/test/java/ru/tp/tree_02/", true);
-        Tree.getTree("./src/test/java/ru/tp/tree_02/", false);
-        Tree.getTree("./src/test/java/", true);
+    public void testForGetTreeMethod1() throws NotDirectoryException, FileNotFoundException {
+        Tree.getTree(middleDirectoryPath.toString(), true);
+
+    }
+
+    @Test
+    public void testForGetTreeMethod2() throws NotDirectoryException, FileNotFoundException {
+        Tree.getTree(middleDirectoryPath.toString(), false);
+    }
+
+    @Test
+    public void testForGetTreeMethod3() throws NotDirectoryException, FileNotFoundException{
+        Tree.getTree(highDirectoryPath.toString(), true);
     }
 
     @Test
@@ -32,14 +71,8 @@ public class TreeTest {
         FileNode node2 = new FileNode();
         node1.isDir = false;
         node2.isDir = true;
-        Tree.filterEmptyNodes(node1, "./src/test/java/ru/tp/tree_02/");
-        Tree.filterEmptyNodes(node2, "./src/test/java/ru/tp/tree_02/");
+        Tree.filterEmptyNodes(node1, middleDirectoryPath.toString());
+        Tree.filterEmptyNodes(node2, middleDirectoryPath.toString());
     }
-
-
-
-
-
-
 
 }
